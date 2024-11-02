b12f44f48c2920a4064bf1d37eddf9447263f61b
md shoharab pk <76746357+web-shoharab-pk@users.noreply.github.com>
2024-11-02 16:18:06 +0600

    Update stripe.ts

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -1,5 +1,6 @@
 import Stripe from 'stripe';
+import { envConfig } from './environment';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+export const stripe = new Stripe(envConfig.STRIPE_SECRET_KEY!, {
   apiVersion: '2024-09-30.acacia',
 });
